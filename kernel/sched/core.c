810c41ba27dad91d514acfd5d8d2af78a6544753
Tejun Heo <tj@kernel.org>
2023-11-14 09:19:47 -1000

    rue/scx/sched_ext: Allow BPF schedulers to switch all eligible tasks into sched_ext
    Upstream: no
    
    Currently, to use sched_ext, each task has to be put into sched_ext using
    sched_setscheduler(2). However, some BPF schedulers and use cases might
    prefer to service all eligible tasks.
    
    This patch adds a new kfunc helper, scx_bpf_switch_all(), that BPF
    schedulers can call from ops.init() to switch all SCHED_NORMAL, SCHED_BATCH
    and SCHED_IDLE tasks into sched_ext. This has the benefit that the scheduler
    swaps are transparent to the users and applications. As we know that CFS is
    not being used when scx_bpf_switch_all() is used, we can also disable hot
    path entry points with static_branches.
    
    Both the simple and qmap example schedulers are updated to switch all tasks
    by default to ease testing. '-p' option is added which enables the original
    behavior of switching only tasks which are explicitly on SCHED_EXT.
    
    v2: In the example schedulers, switch all tasks by default.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Hongbo Li <herberthbli@tencent.com>
    Suggested-by: Barret Rhoden <brho@google.com>
    Reviewed-by: David Vernet <dvernet@meta.com>
    Acked-by: Josh Don <joshdon@google.com>
    Acked-by: Hao Luo <haoluo@google.com>
    Acked-by: Barret Rhoden <brho@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1237,7 +1237,7 @@ bool sched_can_stop_tick(struct rq *rq)
 	 * if there's more than one we need the tick for involuntary
 	 * preemption.
 	 */
-	if (rq->nr_running > 1)
+	if (!scx_switched_all() && rq->nr_running > 1)
 		return false;
 
 	/*
@@ -5748,8 +5748,10 @@ void scheduler_tick(void)
 		wq_worker_tick(curr);
 
 #ifdef CONFIG_SMP
-	rq->idle_balance = idle_cpu(cpu);
-	trigger_load_balance(rq);
+	if (!scx_switched_all()) {
+		rq->idle_balance = idle_cpu(cpu);
+		trigger_load_balance(rq);
+	}
 #endif
 }
 
