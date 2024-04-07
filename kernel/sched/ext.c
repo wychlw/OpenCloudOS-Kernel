59c58fade291fd800e4c00226e7dd845dae4ffe1
Hongbo Li <herberthbli@tencent.com>
2024-04-07 11:21:55 +0800

    rue/scx: support perf runtime
    Add trace point for perf runtime statistic.
    
    Signed-off-by: Hongbo Li <herberthbli@tencent.com>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -605,6 +605,7 @@ static void update_curr_scx(struct rq *rq)
 	delta_exec = now - curr->se.exec_start;
 	curr->se.exec_start = now;
 	curr->se.sum_exec_runtime += delta_exec;
+	trace_sched_stat_runtime(curr, delta_exec, curr->scx.dsq_vtime);
 	account_group_exec_runtime(curr, delta_exec);
 	cgroup_account_cputime(curr, delta_exec);
 
