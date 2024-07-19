7f3aec3c16858ce5f09d4ec4f7aaf9f46b7794f2
Jianping Liu <frankjpliu@tencent.com>
2024-07-19 21:16:14 +0800

    drivers/thirdparty: keep compile bnxt_re if not using thirdparty drivers
    Supporting the case that some one want using kernel native bnxt and
    bnxt_re.
    
    Signed-off-by: Jianping Liu <frankjpliu@tencent.com>
    Reviewed-by: Yongliang Gao <leonylgao@tencent.com>

--- drivers/thirdparty/copy-drivers.sh
+++ drivers/thirdparty/copy-drivers.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 if [ ! -e release-drivers/.git ] ; then
-	timeout 300 git clone https://gitee.com/OpenCloudOS/release-drivers.git release-drivers/
+	## If clone git fail, using the kernel native drivers to compile.
+	timeout 300 git clone https://gitee.com/OpenCloudOS/release-drivers.git release-drivers/ || exit 0
 fi
+
+echo "Having release-drivers git repo, coping the thirdparty driver code to kernel native dir."
 
 if [ -e release-drivers/bnxt ]; then
 	rm -rf ../../drivers/net/ethernet/broadcom/bnxt
 	cp -a release-drivers/bnxt ../../drivers/net/ethernet/broadcom/
+
+	## Use sed to replace "&& BNXT" with "&& BNXT && !THIRDPARTY_BNXT" in
+	## drivers/infiniband/hw/bnxt_re/Kconfig
+	## Because compile kernel native bnxt_re will fail when using thirdparty bnxt.
+	sed -i 's/\(&& BNXT\)$/\1 \&\& !THIRDPARTY_BNXT/g' ../../drivers/infiniband/hw/bnxt_re/Kconfig
 fi
